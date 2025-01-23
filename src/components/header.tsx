c364d64983d565a475d724eb1fa09ac63e0cca90
Raphael Aciardi <109046764+raphael-aciardi@users.noreply.github.com>
2025-01-23 15:35:36 -0300

    add client/server env variables boundary

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { CartWidget } from "./cart-widget";
 import { SearchForm } from "./search-form";
+import { Suspense } from "react";
 
 export function Header() {
 
@@ -11,20 +12,22 @@ export function Header() {
                 devstore
             </Link>
 
-            <SearchForm />
+            <Suspense fallback={null}>
+                <SearchForm />
+            </Suspense>
         </div>
 
-       
+
 
         <div className="flex items-center gap-4">
-            
+
             <CartWidget />
 
             <div className="w-px h-4 bg-zinc-700" />
 
             <Link href='/' className="flex items-center gap-2 hover:underline">
                 <span className="text-sm"> Account</span>
-                <Image src="https://github.com/raphael-aciardi.png" className="h-6 w-6 rounded-full" width={24} height={24} alt="avatar"/>
+                <Image src="https://github.com/raphael-aciardi.png" className="h-6 w-6 rounded-full" width={24} height={24} alt="avatar" />
             </Link>
         </div>
     </div>
